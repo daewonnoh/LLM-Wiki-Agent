0bfef4f95c942714df70af70fdd8af49ba2f83b4
nnovum <naisdw@empas.com>
2026-05-25 20:17:28 +0900

    Fix missing styles for academic paper title section

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 $git = (Get-ChildItem -Path C:\Users\naisd\AppData\Local\GitHubDesktop -Filter git.exe -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.FullName -match 'cmd\\git.exe$' } | Select-Object -First 1).FullName
 if ($git) {
     & $git add .
-    & $git commit -m "Update hero description text on homepage"
+    & $git commit -m "Fix missing styles for academic paper title section"
     & $git push
 } else {
     Write-Output "Git not found"
