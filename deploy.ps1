1a452e62343ea044f6a678332d6354ae34b8607e
nnovum <naisdw@empas.com>
2026-05-25 20:13:48 +0900

    Update hero description text on homepage

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 $git = (Get-ChildItem -Path C:\Users\naisd\AppData\Local\GitHubDesktop -Filter git.exe -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.FullName -match 'cmd\\git.exe$' } | Select-Object -First 1).FullName
 if ($git) {
     & $git add .
-    & $git commit -m "Move fullscreen button to bottom control bar of slide viewer"
+    & $git commit -m "Update hero description text on homepage"
     & $git push
 } else {
     Write-Output "Git not found"
