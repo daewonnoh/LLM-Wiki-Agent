1b165677c25f62df062c7e4954c8940697f21e56
nnovum <naisdw@empas.com>
2026-05-25 20:38:05 +0900

    Fix syntax error and duplicate layout in App.jsx

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 $git = (Get-ChildItem -Path C:\Users\naisd\AppData\Local\GitHubDesktop -Filter git.exe -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.FullName -match 'cmd\\git.exe$' } | Select-Object -First 1).FullName
 if ($git) {
     & $git add .
-    & $git commit -m "Implement subtabs for Media page and Add Thesis Reader quicklink to homepage"
+    & $git commit -m "Fix syntax error and duplicate layout in App.jsx"
     & $git push
 } else {
     Write-Output "Git not found"
