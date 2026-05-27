d4ec222bd74ca1564e1dcb606ed6ee6aae88203e
Dae-won Noh <naisdw@gmail.com>
2026-05-27 13:59:07 +0900

    Fix JSX syntax error to allow build and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -793,7 +793,7 @@ function App() {
                 className={`tab-btn ${activeMediaTab === 'webtoon' ? 'active' : ''}`}
                 onClick={() => setActiveMediaTab('webtoon')}
               >
-                듀나 <그레타 복음> 10컷 소설 웹툰
+                듀나 &lt;그레타 복음&gt; 10컷 소설 웹툰
               </button>
               <button 
                 className={`tab-btn ${activeMediaTab === 'podcast' ? 'active' : ''}`}
